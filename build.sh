b0a2b52f8c96d9dd283c22376bc88ca7ff689ca5
Van-Duyet Le <me@duyetdev.com>
2016-08-30 22:55:57 +0700

    Save local changes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 git commit -am "Save local changes"
 git checkout -B gh-pages
 git add -f build
-echo "contact.duyetdev.com" >> CNAME
+echo "contact.duyetdev.com" > CNAME
 git commit -am "Rebuild website"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
